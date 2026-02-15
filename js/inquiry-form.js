0f157ea4061b9f955e9bde7a1df906e165c7cfa7
Jiratchaya K. <jiratchaya98x@gmail.com>
2026-02-16 04:21:57 +0700

    feat: 🎸 create inquiry section

---
+++ js/inquiry-form.js
@@ -0,0 +1,18 @@
+const onClickSubject = () => {
+  const subjectDropdown = document.querySelector(".subject-dropdown");
+  const dropdownArrow = document.querySelector(".dropdown-arrow");
+  subjectDropdown.classList.toggle("open");
+  dropdownArrow.classList.toggle("open");
+};
+
+const onClickDropdownItem = (event) => {
+  event.preventDefault();
+  const subjectDropdown = document.querySelector(".subject-dropdown");
+  const subjectLabel = document.querySelector("label[for='subject']");
+  const dropdownArrow = document.querySelector(".dropdown-arrow");
+  const subjectInput = document.querySelector("input[name='subject']");
+  subjectLabel.classList.add("inquiry-label");
+  subjectInput.value = event.target.textContent;
+  subjectDropdown.classList.remove("open");
+  dropdownArrow.classList.remove("open");
+};
