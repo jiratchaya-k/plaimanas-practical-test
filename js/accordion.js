74f1dd698a5cfbf32cc3865b933d00c4f18d07de
Jiratchaya K. <jiratchaya98x@gmail.com>
2026-02-17 01:25:38 +0700

    feat: 🎸 faq responsive and handle dynamic height

--- js/accordion.js
+++ js/accordion.js
@@ -1,7 +1,31 @@
 const accordionItem = document.getElementsByClassName("accordion-item");
 
-for (let i = 0; i < accordionItem.length; i++) {
-  accordionItem[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-  });
-}
+$(document).ready(() => {
+  for (let i = 0; i < accordionItem.length; i++) {
+    const currAccordion = accordionItem[i];
+
+    handleAccordionItemHeight(currAccordion);
+
+    $(currAccordion).click(() => {
+      currAccordion.classList.toggle("active");
+      handleAccordionItemHeight(currAccordion);
+    });
+  }
+});
+
+const handleAccordionItemHeight = (element) => {
+  const computedStyle = window.getComputedStyle(element);
+  const contentGapValue = computedStyle.getPropertyValue("gap");
+  const contentGap = Number(contentGapValue.replace("px", ""));
+  const questionHeight =
+    element.getElementsByClassName("accordion-question")[0].offsetHeight;
+  const answerHeight =
+    element.getElementsByClassName("accordion-answer")[0].offsetHeight;
+
+  if (element.classList.contains("active")) {
+    const contentHeight = questionHeight + contentGap + answerHeight;
+    element.style.height = `${contentHeight}px`;
+  } else {
+    element.style.height = `${questionHeight}px`;
+  }
+};
